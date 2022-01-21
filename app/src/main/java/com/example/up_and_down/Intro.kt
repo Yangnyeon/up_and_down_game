bcf648cf70578688d32a0ac194af8514772b10c0
Yangnyeon <tmdgurdla324@naver.com>
2022-01-21 09:40:38 +0900

    연습중..

---
+++ app/src/main/java/com/example/up_and_down/Intro.kt
@@ -0,0 +1,25 @@
+package com.example.up_and_down
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Intro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        var handler = Handler()
+        handler.postDelayed( {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
